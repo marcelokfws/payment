7794c2f19cad94ec318709217f51eca786b43c1d
marcelokfws <marcelokfws@gmail.com>
2023-08-31 22:29:56 -0300

    first commit

---
+++ payments/views.py
@@ -0,0 +1,28 @@
+
+import stripe
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):  # new
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        return context
+
+
+def charge(request):
+    if request.method == "POST":
+        charge = stripe.Charge.create(
+            amount=500,
+            currency='usd',
+            description='Payment Gateway',
+            source=request.POST['stripeToken']
+
+        )
+        return render(request, 'charge.html')
